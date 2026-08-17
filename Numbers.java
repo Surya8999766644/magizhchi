bf930651145edee98a2af25931c5477ee9334297
Surya <surya01082006@gmail.com>
2026-08-17 07:32:41 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers 
+{
+	public static void main(String[] args)throws InterruptedException 
+	{
+	for (int i = 1; i <= 5 ; i++)
+		{
+		System.out.println(i);
+		Thread.sleep(1000); // 1 second dealy
+	}
+	}
+}
+		
+	
